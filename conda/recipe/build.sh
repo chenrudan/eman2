788e2e8a2575bb6b0f4d94e15461483d1793d5f6
James Michael Bell <jmbell.net@gmail.com>
2016-02-07 10:19:58 -0600

    altered FREETYPE include path in CMakeLists

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -2,7 +2,13 @@
 
 #http://conda-test.pydata.org/docs/build.html
 
-cmake $SRC_DIR
-make
+# CMAKE VARS
+#cmake BOOST_INCLUDE_PATH=$PREFIX/include BOOST_LIBRARY=$PREFIX/lib/libboost_python.dylib FREETYPE_INCLUDE_PATH=$PREFIX/include/freetype2 NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include PYTHON_INCLUDE_PATH=$PREFIX/include/python2.7 EMAN_INSTALL_PREFIX=$PREFIX $SRC_DIR
+
+cmake NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include PYTHON_INCLUDE_PATH=$PREFIX/include/python2.7 EMAN_INSTALL_PREFIX=$PREFIX $SRC_DIR
+
+echo "$(cmake -LAH | grep FREETYPE)"
+
+make -j8
 make install
 
