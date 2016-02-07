ca8f51e60911ea71c53a64beaa91e745a16522fa
James Michael Bell <jmbell.net@gmail.com>
2016-02-07 11:00:24 -0600

    fixed conda recipe

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-#http://conda-test.pydata.org/docs/build.html
-
-# CMAKE VARS
-#cmake BOOST_INCLUDE_PATH=$PREFIX/include BOOST_LIBRARY=$PREFIX/lib/libboost_python.dylib FREETYPE_INCLUDE_PATH=$PREFIX/include/freetype2 NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include PYTHON_INCLUDE_PATH=$PREFIX/include/python2.7 EMAN_INSTALL_PREFIX=$PREFIX $SRC_DIR
-
-cmake NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include PYTHON_INCLUDE_PATH=$PREFIX/include/python2.7 EMAN_INSTALL_PREFIX=$PREFIX $SRC_DIR
-
-echo "$(cmake -LAH | grep FREETYPE)"
-
-make -j8
+cmake NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include PYTHON_INCLUDE_PATH=$PREFIX/include/python2.7 EMAN_INSTALL_PREFIX=$PREFIX/EMAN2 $SRC_DIR
+make -j4
 make install
 
