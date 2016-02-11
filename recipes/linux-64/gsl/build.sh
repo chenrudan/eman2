c2607d5497024e8ca62113f1675707b02ba48f74
James Michael Bell <jmbell91@gmail.com>
2016-02-10 23:52:43 -0600

    added gsl linux x86_64 recipe

--- recipes/linux-64/gsl/build.sh
+++ recipes/linux-64/gsl/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I$PREFIX/include"
-export CXXFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-
 ./configure --prefix=$PREFIX --enable-shared --disable-static
-
 make
-make check
 make install
+
