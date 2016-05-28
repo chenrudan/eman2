20cc36c509bb334f78b57d9142b0b8f18229b2d3
James Michael Bell <jmbell.net@gmail.com>
2016-05-28 14:48:16 -0500

    updated to work across linux and mac

--- recipes/osx-64/eman2/build.sh
+++ recipes/osx-64/eman2/build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-cmake NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include PYTHON_INCLUDE_PATH=$PREFIX/include/python2.7 EMAN_INSTALL_PREFIX=$PREFIX CMAKE_SKIP_RPATH=ON $SRC_DIR
+case "$(uname -s)" in
+	Darwin*)  OS="mac" ;; 
+	Linux*)   OS="linux" ;;
+	*)        OS="other" ;;
+esac
+
+if [[ $OS == "linux" ]]; then
+	cmake NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include PYTHON_INCLUDE_PATH=$PREFIX/include/python2.7 EMAN_INSTALL_PREFIX=$PREFIX $SRC_DIR
+elif [[ $OS == "mac" ]]; then
+	cmake NUMPY_INCLUDE_PATH=$SP_DIR/numpy/core/include PYTHON_INCLUDE_PATH=$PREFIX/include/python2.7 EMAN_INSTALL_PREFIX=$PREFIX CMAKE_SKIP_RPATH=ON $SRC_DIR
+fi
+
 make -j"${CPU_COUNT}"
 make install
 
