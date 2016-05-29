5f40adfc19420266e172adde9ed89be76db17899
James Michael Bell <jmbell.net@gmail.com>
2016-05-29 16:19:48 -0500

    moved prelink content to postlink

--- recipes/osx-64/eman2/post-link.sh
+++ recipes/osx-64/eman2/post-link.sh
@@ -2,3 +2,23 @@
 
 echo "Performing post-linking tasks..."
 
+case "$(uname -s)" in
+  Darwin*)  OS="osx" ;; 
+  Linux*)   OS="linux" ;;
+  *)        OS="other" ;;
+esac
+
+case "$(uname -m)" in
+  x86_64)  ARCH="64" ;;
+  i686)    ARCH="32" ;;
+  *)       ARCH="other" ;;
+esac
+
+NAME="${PKG_NAME}-${PKG_VERSION}"
+FULLNAME="${NAME}-${PKG_BUILDNUM}"
+RECIPE="${PREFIX}/pkgs/${FULLNAME}/info/recipe"
+TARGET="${OS}-${ARCH}"
+
+echo "Installing ${NAME} to ${PREFIX} on ${TARGET} ($(date))"
+
+python ${RECIPE}/fix-libs.py --target=${TARGET} --prefix=${PREFIX} --dist=${NAME}
