73b0be1904bfccee71b49ffc40ae024066d137ef
shadow_walker <shadowwalkersb@gmail.com>
2016-07-25 20:57:04 -0500

    setup.py: refactor, update

--- recipes/eman2/setup.py
+++ recipes/eman2/setup.py
@@ -1,19 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import glob
+import os
 
-scripts = [name for name in glob.glob('lib/bin/*.py')]
+libdir='lib'
+sx_scripts = [name for name in glob.glob(os.path.join(libdir,'bin','*.py'))]
+e2_scripts = [name for name in glob.glob(os.path.join(libdir,'bin','*.py'))]
+
+e2_pkgdata = ['*.so','*.dylib','pmconfig/*','pyemtbx/*','include/*.h','include/gorgon/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py']
+
+sx_pkgdata = ['*.so','*.dylib','pyemtbx/*','include/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py']
 
 setup(
 	name="eman2",
 	version="3.0.0",
 
-    package_dir={'':'lib',
-                 'bin':'lib/bin',
-                 'lib':'lib/EMAN2'},
-    packages=find_packages(),
-    package_data={'':['*.so']},
-
-    scripts=scripts,
+    package_dir={'EMAN2':os.path.join(libdir,'EMAN2'),'sparx':os.path.join(libdir,'sparx')},
+    packages=['EMAN2','sparx'],
+    package_data={'EMAN2':e2_pkgdata,'sparx':sx_pkgdata},
+    include_package_data = True,
+    scripts = e2_scripts + sx_scripts,
     
     zip_safe = False,
 
