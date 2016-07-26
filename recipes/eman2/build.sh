40e728a3942d49009370a04fa831acbf7e6efcff
shadow_walker <shadowwalkersb@gmail.com>
2016-07-25 20:16:32 -0500

    cleanup build.sh

--- recipes/eman2/build.sh
+++ recipes/eman2/build.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-case "$(uname -s)" in
-	Darwin*)  OS="mac" ;; 
-	Linux*)   OS="linux" ;;
-	*)        OS="other" ;;
-esac
+build_dir="${SRC_DIR}/../build_dir"
 
-PY_INC=${PREFIX}/include/python2.7
-NPY_INC=${PREFIX}/lib/python2.7/site-packages/numpy/core/include
+mkdir -p $build_dir
+cd $build_dir
 
-#echo "PYTHON_INCLUDE_PATH: ${PY_INC}"
-#echo "NUMPY_INCLUDE_PATH: ${NPY_INC}"
-#echo "OS: ${OS}"
-#echo "SRC_DIR: ${SRC_DIR}"
-#echo "SP_DIR: ${SP_DIR}"
-#echo "PREFIX: ${PREFIX}"
-
-if [[ $OS == "linux" ]]; then
-	cmake -DNUMPY_INCLUDE_PATH=${NPY_INC} -DPYTHON_INCLUDE_PATH=${PY_INC} -DEMAN_INSTALL_PREFIX=${PREFIX} -DCMAKE_SKIP_RPATH=ON ${SRC_DIR}
-elif [[ $OS == "mac" ]]; then
-	cmake -DNUMPY_INCLUDE_PATH=${NPY_INC} -DPYTHON_INCLUDE_PATH=${PY_INC} -DEMAN_INSTALL_PREFIX=${PREFIX} -DCMAKE_SKIP_RPATH=ON $SRC_DIR
-fi
+cmake $SRC_DIR
 
 make -j"$(bc -l <<< ${CPU_COUNT}-2)"
 make install
