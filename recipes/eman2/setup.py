2c54bfb64e07f8b54c672def373fdb02be6f8850
shadow_walker <shadowwalkersb@gmail.com>
2016-07-27 16:00:28 -0500

    update path in setup.py

--- recipes/eman2/setup.py
+++ recipes/eman2/setup.py
@@ -7,7 +7,7 @@ setup(
     name="eman2",
     version="2.2",
 
-    package_dir={'EMAN2':'libeman/EMAN2'},
+    package_dir={'EMAN2':'lib/EMAN2'},
     # packages=find_packages('EMAN2'),
     packages={'EMAN2':'EMAN2',
               'EMAN2.qtgui':'EMAN2.qtgui',
@@ -21,6 +21,6 @@ setup(
     #         'gorgon=run.gorgon:main',
     #     ],
     # },
-    
+
     zip_safe = False,
 )
