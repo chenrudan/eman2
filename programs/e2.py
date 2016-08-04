062b0262ade91e62f97d18a755972f169153ea6c
James Michael Bell <jmbell.net@gmail.com>
2016-08-04 09:47:41 -0500

    attempting to get rid of EMAN2 environment variable by creating it at runtime in the __init__.py script in the EMAN2 site packages directory.

--- programs/e2.py
+++ programs/e2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import os
+#import os
+from EMAN2 import * # we define EMAN2DIR in __init__.py and import os
 
 e2real=os.getenv("EMAN2DIR")+"/bin/e2_real.py"
 os.execlp("ipython","ipython","-i",e2real)
+
