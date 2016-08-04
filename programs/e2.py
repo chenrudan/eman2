141204717f43d92dfea8cf422a5d2b867e1c52fc
shadow_walker <shadowwalkersb@gmail.com>
2016-08-04 17:39:23 -0500

    update, cleanup: e2.py

--- programs/e2.py
+++ programs/e2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-#import os
-from EMAN2 import * # we define EMAN2DIR in __init__.py and import os
+import os
+import e2_real
 
-e2real=os.getenv("EMAN2DIR")+"/bin/e2_real.py"
-os.execlp("ipython","ipython","-i",e2real)
-
+os.execlp("ipython","ipython","-i","e2_real.py")
