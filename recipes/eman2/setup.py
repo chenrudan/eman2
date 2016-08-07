cd5fe0fadc9693395f0ae32622480b36f2d6a0e0
James Michael Bell <jmbell@mbp.local>
2016-08-07 02:10:08 -0500

    cmake: preifx with ${EMAN_INSTALL_PREFIX}/EMAN2 sparx, setup.py

--- recipes/eman2/setup.py
+++ recipes/eman2/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 import glob
 import os
 
-libdir='libeman'
+libdir='lib'
 sx_scripts = [name for name in glob.glob(os.path.join(libdir,'EMAN2','bin','*.py'))]
 e2_scripts = [name for name in glob.glob(os.path.join(libdir,'sparx','bin','*.py'))]
 
-e2_package_data = ['*.so','*.dylib','pmconfig/*','pyemtbx/*','include/*.h','include/gorgon/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py','bin/*.py']
+e2_pkgdata = ['*.so','*.dylib','pmconfig/*','pyemtbx/*','include/*.h','include/gorgon/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py']
 
-sx_package_data = ['*.so','*.dylib','pyemtbx/*','include/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py','bin/*.py']
+sx_pkgdata = ['*.so','*.dylib','pyemtbx/*','include/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py']
 
 setup(
 	name="eman2",
@@ -16,7 +16,7 @@ setup(
 
     package_dir={'EMAN2':'libeman/EMAN2','sparx':'libeman/sparx'},
     packages=['EMAN2','sparx'],
-    package_data={'EMAN2':e2_package_data,'sparx':sx_package_data},
+    package_data={'EMAN2':e2_pkgdata,'sparx':sx_pkgdata},
     include_package_data = True,
     scripts = e2_scripts + sx_scripts,
     
