ae1df3d9c68c43090c9bcf178f3d5e5090b60d1e
James Michael Bell <jmbell@mbp.local>
2016-08-07 16:06:13 -0500

    generalized e2.py for EMAN2/sparx python library

--- programs/e2.py
+++ programs/e2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import os
-#import sys
 import site
 
-e2real = "{}/eman2-3.0.0-py2.7.egg-info/scripts/e2_real.py".format(site.getsitepackages()[0])
-#e2real="{}/bin/e2_real.py".format(sys.exec_prefix)
-#e2real="{}/bin/e2_real.py".format(os.getenv("EMAN2DIR"))
+spdir = site.getsitepackages()[0] # current environment's site packages directory
+egginf = max([i for i in os.listdir(spdir) if "eman2" in i if "egg-info" in i]) # get most recent version if multiple are present
+e2real = "{}/{}/scripts/e2_real.py".format(spdir,egginfo)
+
 os.execlp("ipython","ipython","-i",e2real)
 
