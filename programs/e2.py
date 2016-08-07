804cb3913d1a39dfd0d581d5d242789711fd736e
James Michael Bell <jmbell@mbp.local>
2016-08-07 11:51:40 -0500

    updated e2.py for anaconda

--- programs/e2.py
+++ programs/e2.py
@@ -2,5 +2,6 @@
 
 import os
 
-e2real=os.getenv("EMAN2DIR")+"/bin/e2_real.py"
+e2real="{}/bin/e2_real.py".format(sys.exec_prefix) #os.getenv("EMAN2DIR")+"/bin/e2_real.py"
 os.execlp("ipython","ipython","-i",e2real)
+
