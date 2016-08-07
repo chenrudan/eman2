6c3e10c0dc974a0b18cfb1ec09650293255da727
James Michael Bell <jmbell@mbp.local>
2016-08-07 15:53:30 -0500

    longer term solution for e2.py, but still not quite general enough

--- programs/e2.py
+++ programs/e2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import os
-import sys
+#import sys
+import site
 
-e2real="{}/bin/e2_real.py".format(sys.exec_prefix)
+e2real = "{}/eman2-3.0.0-py2.7.egg-info/scripts/e2_real.py".format(site.getsitepackages()[0])
+#e2real="{}/bin/e2_real.py".format(sys.exec_prefix)
 #e2real="{}/bin/e2_real.py".format(os.getenv("EMAN2DIR"))
 os.execlp("ipython","ipython","-i",e2real)
 
