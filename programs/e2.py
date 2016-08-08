c7bad54a8d80d75bf17dae074361b35d12495746
James Michael Bell <jmbell@mbp.local>
2016-08-08 16:47:00 -0500

    fix for EMAN2DIR

--- programs/e2.py
+++ programs/e2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 
 import os
-import site
 
-spdir = site.getsitepackages()[0] # current environment's site packages directory
-egginfo = max([i for i in os.listdir(spdir) if "eman2" in i if "egg-info" in i]) # get most recent version if multiple are present
-e2real = "{}/{}/scripts/e2_real.py".format(spdir,egginfo)
-
+e2real = "{}/e2_real.py".format(os.path.dirname(os.path.abspath(__file__)))
 os.execlp("ipython","ipython","-i",e2real)
 
