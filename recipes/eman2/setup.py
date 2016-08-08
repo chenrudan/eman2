04aee99031723d65f4aa51b787122622522cecc3
James Michael Bell <jmbell@mbp.local>
2016-08-08 15:03:59 -0500

    updated setup for separate eman2/sparx libs

--- recipes/eman2/setup.py
+++ recipes/eman2/setup.py
@@ -4,6 +4,7 @@ import glob
 #scripts = [name for name in glob.glob('lib/bin/*.py')] # separated EMAN and sparx programs into respective libs
 sx_scripts = [name for name in glob.glob('libeman/EMAN2/bin/*.py')]
 e2_scripts = [name for name in glob.glob('libeman/sparx/bin/*.py')]
+scripts = sx_scripts + e2_scripts
 
 e2_package_data = ['*.so','*.dylib','pmconfig/*','pyemtbx/*','include/*.h','include/gorgon/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py','bin/*.py']
 
