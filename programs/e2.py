9ef481495d143a70fd9614087932469db9d64731
James Michael Bell <jmbell@mbp.local>
2016-08-08 09:24:59 -0500

    fixed bug in e2.py

--- programs/e2.py
+++ programs/e2.py
@@ -4,7 +4,7 @@ import os
 import site
 
 spdir = site.getsitepackages()[0] # current environment's site packages directory
-egginf = max([i for i in os.listdir(spdir) if "eman2" in i if "egg-info" in i]) # get most recent version if multiple are present
+egginfo = max([i for i in os.listdir(spdir) if "eman2" in i if "egg-info" in i]) # get most recent version if multiple are present
 e2real = "{}/{}/scripts/e2_real.py".format(spdir,egginfo)
 
 os.execlp("ipython","ipython","-i",e2real)
