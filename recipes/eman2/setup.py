8aa510c949de5388ff61bda1f10ff2c2f6486044
jamesmbell <jmbell91@gmail.com>
2016-08-09 10:59:33 -0500

    updates primarily to cmake lists for linux

--- recipes/eman2/setup.py
+++ recipes/eman2/setup.py
@@ -6,9 +6,9 @@ sx_scripts = [name for name in glob.glob('libeman/EMAN2/bin/*.py')]
 e2_scripts = [name for name in glob.glob('libeman/sparx/bin/*.py')]
 scripts = sx_scripts + e2_scripts
 
-e2_package_data = ['*.so','*.dylib','pmconfig/*','pyemtbx/*','include/*.h','include/gorgon/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py','bin/*.py']
+e2_pkgdata = ['*.so','*.dylib','pmconfig/*','pyemtbx/*','include/*.h','include/gorgon/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py','bin/*.py']
 
-sx_package_data = ['*.so','*.dylib','pyemtbx/*','include/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py','bin/*.py']
+sx_pkgdata = ['*.so','*.dylib','pyemtbx/*','include/*.h','include/plugins/*.h','include/sparx/*.h','examples/*.py','fonts/*.txt','fonts/*.ttf','images/*.png','images/*.ico','images/macimages/*.png','doc/*','test/rt/*.py','bin/*.py']
 
 setup(
     name="eman2",
@@ -20,7 +20,7 @@ setup(
     url="http://blake.bcm.edu/emanwiki/EMAN2",
     package_dir={'EMAN2':'libeman/EMAN2','sparx':'libeman/sparx'},
     packages=['EMAN2','sparx'],
-    package_data={'EMAN2':e2_package_data,'sparx':sx_package_data},
+    package_data={'EMAN2':e2_pkgdata,'sparx':sx_pkgdata},
     include_package_data = True,
     scripts=scripts,
     zip_safe = False,
