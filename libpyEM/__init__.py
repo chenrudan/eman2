a9ef4fc2919a2e1ed3765ba841b957cbc309a13a
James Michael Bell <jmbell91@gmail.com>
2016-08-09 18:20:49 -0500

    updated cmake lists

--- libpyEM/__init__.py
+++ libpyEM/__init__.py
@@ -1754,10 +1754,8 @@ def get_3d_font_renderer():
 		font_renderer.set_using_display_lists(True)
 		font_renderer.set_depth(2)
 		pfm = get_platform()
-		if pfm in ["Linux","Darwin"]:
-			font_renderer.set_font_file_name(os.getenv("EMAN2DIR")+"/fonts/DejaVuSerif.ttf")
-		elif pfm == "Windows":
-			font_renderer.set_font_file_name("C:\\WINDOWS\\Fonts\\arial.ttf")
+		if pfm in ["Linux","Darwin"]: font_renderer.set_font_file_name("{}/fonts/DejaVuSerif.ttf".format(os.getenv("EMAN2DIR")))
+		elif pfm == "Windows": font_renderer.set_font_file_name("C:\\WINDOWS\\Fonts\\arial.ttf")
 		else:
 			print "unknown platform:",pfm
 		return font_renderer
